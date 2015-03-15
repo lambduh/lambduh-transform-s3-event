d2adee00dd4fa1329e385630d5a77393f1f8781a
Russell Matney <russell.matney@gmail.com>
2015-03-15 12:05:39 -0400

    tests for handling s3 json, passing options, etc.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 Q = require('q');
 
 module.exports = function(s3Event) {
-  return function() {
+  return function(options) {
+    if (!options) { options = {} }
     return Q.promise(function(resolve, reject) {
-      if (s3Event) {
+      if (s3Event &&
+          s3Event.Records &&
+          s3Event.Records[0] &&
+          s3Event.Records[0].s3 &&
+          s3Event.Records[0].s3.bucket &&
+          s3Event.Records[0].s3.bucket.name &&
+          s3Event.Records[0].s3.object &&
+          s3Event.Records[0].s3.object.key
+         ) {
+        options.srcBucket = s3Event.Records[0].s3.bucket.name
+        options.srcKey = s3Event.Records[0].s3.object.key
+
+        resolve(options)
       } else {
-        reject(new Error('Passed S3 Event could not be transformed'));
+        reject(new Error('Inputted S3 Event could not be transformed'));
       }
     });
   };
--- test/transform-s3-event.spec.js
+++ test/transform-s3-event.spec.js
@@ -15,14 +15,72 @@ describe('transformS3Event', function() {
     expect(transform()().then).to.exist;
   });
 
-  it('should throw an error if the input event is not as expected', function(done) {
+  it('should throw an error if the input event is null', function(done) {
     var event = null;
     transform(event)().then(function() {
       done(new Error('This event should have thrown an error.'));
-    }, function() {
+    }, function(err) {
+      expect(err).to.exist
       done();
     });
   });
+
+  it('should throw an error if the input event is not an s3 Event', function(done) {
+    var event = {};
+    transform(event)().then(function() {
+      done(new Error('This event should have thrown an error.'));
+    }, function(err) {
+      expect(err).to.exist
+      done();
+    });
+  });
+
+  it('(with proper input) should attach the srcBucket and srcKey to an inputted options object', function(done) {
+    var event = require('./good-input.json')
+    transform(event)().then(function(options) {
+      if (options && options.srcBucket && options.srcKey) {
+        expect(options.srcBucket).to.exist.and.to.be.a('string')
+        expect(options.srcKey).to.exist.and.to.be.a('string')
+        done()
+      } else {
+        done(new Error('Expected options.srcBucket, options.srcKey to exist'));
+      }
+    }, function(err) {
+      done(err);
+    });
+  });
+
+  it('(with proper input) should resolve the inputted options object', function(done) {
+    var event = require('./good-input.json')
+    var inputOptions = {
+      key: 'value'
+    }
+    transform(event)(inputOptions).then(function(options) {
+      if (options && options.key) {
+        expect(options.key).to.equal('value')
+        expect(options).to.equal(inputOptions)
+        done()
+      } else {
+        done(new Error('Expected resolved options object to match input options'));
+      }
+    }, function(err) {
+      done(err);
+    });
+  });
+
+  it('(with proper input) should resolve a new options object if none is input', function(done) {
+    var event = require('./good-input.json')
+    transform(event)().then(function(options) {
+      if (options) {
+        done()
+      } else {
+        done(new Error('Expected options object to be resolved'));
+      }
+    }, function(err) {
+      done(err);
+    });
+  });
+
 
 
 });
