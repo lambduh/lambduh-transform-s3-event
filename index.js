b05e694f1dae38c07059b98b2e7ad43530f947a2
Russell Matney <russell.matney@gmail.com>
2015-03-15 13:03:23 -0400

    promise refactor

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 Q = require('q');
 
 module.exports = function(s3Event) {
+
   return function(options) {
     if (!options) { options = {} }
-    return Q.promise(function(resolve, reject) {
-      if (s3Event &&
-          s3Event.Records &&
-          s3Event.Records[0] &&
-          s3Event.Records[0].s3 &&
-          s3Event.Records[0].s3.bucket &&
-          s3Event.Records[0].s3.bucket.name &&
-          s3Event.Records[0].s3.object &&
-          s3Event.Records[0].s3.object.key
-         ) {
-        options.srcBucket = s3Event.Records[0].s3.bucket.name
-        options.srcKey = s3Event.Records[0].s3.object.key
+    var def = Q.defer();
 
-        resolve(options)
-      } else {
-        reject(new Error('Inputted S3 Event could not be transformed'));
-      }
-    });
+    if (s3Event &&
+        s3Event.Records &&
+        s3Event.Records[0] &&
+        s3Event.Records[0].s3 &&
+        s3Event.Records[0].s3.bucket &&
+        s3Event.Records[0].s3.bucket.name &&
+        s3Event.Records[0].s3.object &&
+        s3Event.Records[0].s3.object.key
+        ) {
+      options.srcBucket = s3Event.Records[0].s3.bucket.name
+      options.srcKey = s3Event.Records[0].s3.object.key
+
+      def.resolve(options)
+    } else {
+      def.reject(new Error('Inputted S3 Event could not be transformed'));
+    }
+
+    return def.promise;
   };
 }
