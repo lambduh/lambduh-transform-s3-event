e2aacfcf9a85461f3fa49bcf60ef7c0f8eeda037
Russell Matney <russell.matney@gmail.com>
2015-03-15 11:15:22 -0400

    first test

---
+++ test/transform-s3-event.spec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+var transform = require('../');
+
+describe('transformS3Event', function() {
+  it('should exist', function() {
+    expect(transform).to.exist;
+  });
+});
